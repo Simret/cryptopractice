d1cc6cefffd21995954121b6499b5306900b57cd
Simret Kassahun <simruineb@gmail.com>
2024-10-09 22:31:12 +0300

    Transaction added

--- Naivecoin/README.md
+++ Naivecoin/README.md
@@ -1,26 +1,31 @@
-# Naivecoin: chapter 1
-The branch for Naivecoin, chapter1
-
 ```
 npm install
-npm start
+npm dev start
 ```
 
 ##### Get blockchain
 ```
-curl http://localhost:3001/blocks
+curl http://localhost:5173/blocks
 ```
 
-##### Create block
+##### Mine a block
 ```
-curl -H "Content-type:application/json" --data '{"data" : "Some data to the first block"}' http://localhost:3001/mineBlock
+curl -X POST http://localhost:5173/mineBlock
 ``` 
 
-##### Add peer
+##### Send transaction
 ```
-curl -H "Content-type:application/json" --data '{"peer" : "ws://localhost:6001"}' http://localhost:3001/addPeer
+curl -H "Content-type: application/json" --data '{"address": "04bfcab8722991ae774db48f934ca79cfb7dd991229153b9f732ba5334aafcd8e7266e47076996b55a14bf9913ee3145ce0cfc1372ada8ada74bd287450313534b", "amount" : 35}' http://localhost:5173/sendTransaction
 ```
-#### Query connected peers
+
+##### Mine transaction
 ```
-curl http://localhost:3001/peers
+curl -H "Content-type: application/json" --data '{"address": "04bfcab8722991ae774db48f934ca79cfb7dd991229153b9f732ba5334aafcd8e7266e47076996b55a14bf9913ee3145ce0cfc1372ada8ada74bd287450313534b", "amount" : 35}' http://localhost:5173/mineTransaction
 ```
+
+
+#### Query information about a specific address
+```
+curl http://localhost:5173/address/04f72a4541275aeb4344a8b049bfe2734b49fe25c08d56918f033507b96a61f9e3c330c4fcd46d0854a712dc878b9c280abe90c788c47497e06df78b25bf60ae64
+```
+
--- Naivecoin/src/main.ts
+++ Naivecoin/src/main.ts
@@ -2,28 +2,34 @@ import * as  bodyParser from 'body-parser';
 import * as express from 'express';
 
 import {Block, generateNextBlock, getBlockchain} from './blockchain';
-import {connectToPeers, getSockets, initP2PServer} from './p2p';
 
-const httpPort: number = parseInt(process.env.HTTP_PORT) || 3001;
-const p2pPort: number = parseInt(process.env.P2P_PORT) || 6001;
+const httpPort: number = parseInt(process.env.HTTP_PORT) || 5173;
 
-const initHttpServer = ( myHttpPort: number ) => {
+
+const initHttpServer = (myHttpPort: number) => {
     const app = express();
     app.use(bodyParser.json());
+
+    app.use((err, req, res, next) => {
+        if (err) {
+            res.status(400).send(err.message)
+        }
+    });
 
     app.get('/blocks', (req, res) => {
         res.send(getBlockchain());
     });
     app.post('/mineBlock', (req, res) => {
+        if (req.body.data == null) {
+            res.send('data parameter is missing');
+            return;
+        }
         const newBlock: Block = generateNextBlock(req.body.data);
-        res.send(newBlock);
-    });
-    app.get('/peers', (req, res) => {
-        res.send(getSockets().map(( s: any ) => s._socket.remoteAddress + ':' + s._socket.remotePort));
-    });
-    app.post('/addPeer', (req, res) => {
-        connectToPeers(req.body.peer);
-        res.send();
+        if (newBlock === null) {
+            res.status(400).send('could not generate block');
+        } else {
+            res.send(newBlock);
+        }
     });
 
     app.listen(myHttpPort, () => {
@@ -32,4 +38,3 @@ const initHttpServer = ( myHttpPort: number ) => {
 };
 
 initHttpServer(httpPort);
-initP2PServer(p2pPort);
